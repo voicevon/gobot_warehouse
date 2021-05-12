a236684a28468b35860439125dd3a7bfd35b8f29
Xuming <voicevon@gmail.com>
2021-05-12 10:58:39 +0800

    draw axis

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 # from typing_extensions import runtime_checkable
 import cv2
-# import numpy as np
+import numpy as np
+from math import sin, cos
 # import threading
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -101,6 +102,40 @@ class WarehouseRobot():
             set_gpio(y_step_pin, LOW)
             delay_us(100)
 
+
+    def draw_axis(img, yaw, pitch, roll, tdx=None, tdy=None, size = 100):
+
+        pitch = pitch * np.pi / 180
+        yaw = -(yaw * np.pi / 180)
+        roll = roll * np.pi / 180
+
+        if tdx != None and tdy != None:
+            tdx = tdx
+            tdy = tdy
+        else:
+            height, width = img.shape[:2]
+            tdx = width / 2
+            tdy = height / 2
+
+        # X-Axis pointing to right. drawn in red
+        x1 = size * (cos(yaw) * cos(roll)) + tdx
+        y1 = size * (cos(pitch) * sin(roll) + cos(roll) * sin(pitch) * sin(yaw)) + tdy
+
+        # Y-Axis | drawn in green
+        #        v
+        x2 = size * (-cos(yaw) * sin(roll)) + tdx
+        y2 = size * (cos(pitch) * cos(roll) - sin(pitch) * sin(yaw) * sin(roll)) + tdy
+
+        # Z-Axis (out of the screen) drawn in blue
+        x3 = size * (sin(yaw)) + tdx
+        y3 = size * (-cos(yaw) * sin(pitch)) + tdy
+
+        cv2.line(img, (int(tdx), int(tdy)), (int(x1),int(y1)),(0,0,255),3)
+        cv2.line(img, (int(tdx), int(tdy)), (int(x2),int(y2)),(0,255,0),3)
+        cv2.line(img, (int(tdx), int(tdy)), (int(x3),int(y3)),(255,0,0),2)
+
+        return img 
+
     def spin_once(self):
         # Take a picture from camera
         image = self.take_picture()
@@ -144,9 +179,11 @@ class WarehouseRobot():
                 cv2.putText(image, str(markerID),
                     (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
+
+                img_marker = self.draw_axis(image,0,0,0)
                 print("[INFO] ArUco marker ID: {}".format(markerID))
                 # show the output image
-                g_mqtt.publish_cv_image('gobot_stonehouse/eye/marker', image)
+                g_mqtt.publish_cv_image('gobot_stonehouse/eye/marker', img_marker)
                 # cv2.imshow("Image", image)
                 cv2.waitKey(0)
         # Get perspective views the plane
