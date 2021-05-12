f9d89d0241c6e431ef8da81ae53190089a6094ab
Xuming <voicevon@gmail.com>
2021-05-12 10:47:06 +0800

    draw marker

--- main.py
+++ main.py
@@ -103,15 +103,52 @@ class WarehouseRobot():
 
     def spin_once(self):
         # Take a picture from camera
-        img = self.take_picture()
-        g_mqtt.publish_cv_image('gobot_stonehouse/eye/origin', img)
+        image = self.take_picture()
+        g_mqtt.publish_cv_image('gobot_stonehouse/eye/origin', image)
 
         # Get corners position from detecting aruco marks
         # aruco_mark = cv2.detect_aruco(img)
         arucoDict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_1000)
         arucoParams = cv2.aruco.DetectorParameters_create()
-        (corners, ids, rejected) = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
-        print(corners, ids, rejected)        
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+        # print(corners, ids, rejected)    
+
+        # verify *at least* one ArUco marker was detected
+        if len(corners) > 0:
+            # flatten the ArUco IDs list
+            ids = ids.flatten()
+            # loop over the detected ArUCo corners
+            for (markerCorner, markerID) in zip(corners, ids):
+                # extract the marker corners (which are always returned in
+                # top-left, top-right, bottom-right, and bottom-left order)
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))    
+                print(markerID, topLeft, bottomRight, bottomLeft, topLeft)
+
+
+                # draw the bounding box of the ArUCo detection
+                cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+                # compute and draw the center (x, y)-coordinates of the ArUco marker
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+                # draw the ArUco marker ID on the image
+                cv2.putText(image, str(markerID),
+                    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)
+                print("[INFO] ArUco marker ID: {}".format(markerID))
+                # show the output image
+                g_mqtt.publish_cv_image('gobot_stonehouse/eye/marker', image)
+                # cv2.imshow("Image", image)
+                cv2.waitKey(0)
         # Get perspective views the plane
         #perspective_img = cv2.get_perspective_image(img, aruco_mark)
         
