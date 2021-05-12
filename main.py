1f113d047c7f747c567a27d0116f7605162be21c
xuming <voicevon@gmail.com>
2021-05-12 04:12:21 +0100

    minus

--- main.py
+++ main.py
@@ -167,18 +167,18 @@ class WarehouseRobot():
 
 
                 # draw the bounding box of the ArUCo detection
-                cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-                cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-                cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-                cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+                #cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+                #cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+                #cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+                #cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
                 # compute and draw the center (x, y)-coordinates of the ArUco marker
                 cX = int((topLeft[0] + bottomRight[0]) / 2.0)
                 cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-                cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+                #cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
                 # draw the ArUco marker ID on the image
-                cv2.putText(image, str(markerID),
-                    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (0, 255, 0), 2)
+                #cv2.putText(image, str(markerID),
+                #    (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                #    0.5, (0, 255, 0), 2)
 
                 img_marker = self.draw_axis(image,0,0,0)
                 print("[INFO] ArUco marker ID: {}".format(markerID))
