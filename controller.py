9966e986aab17c1d4c28b436f5d75b8da594c191
Xuming <voicevon@gmail.com>
2021-05-12 15:43:17 +0800

    retab

--- controller.py
+++ controller.py
@@ -17,39 +17,33 @@ class Controller():
 		GPIO.setup(self.__y_step_pin, GPIO.OUT)
 
    	def move_stone(self, relative_x, relative_y):
-        x_dir = GPIO.HIGH
-        if relative_x < 0:
-            x_dir = GPIO.LOW
-            relative_x = - relative_x
+		x_dir = GPIO.HIGH
+		if relative_x < 0:
+			x_dir = GPIO.LOW
+			relative_x = - relative_x
 		GPIO.output(self.__x_dir_pin, x_dir)
 
-        y_dir = GPIO.HIGH
-        if relative_y < 0:
-            y_dir = GPIO.LOW
-            relative_y = - relative_y
+		y_dir = GPIO.HIGH
+		if relative_y < 0:
+			y_dir = GPIO.LOW
+			relative_y = - relative_y
 		GPIO.output(self.__y_dir_pin, y_dir)
-        # set_gpio(y_dir_pin,y_dir)
 
 
-        max_step = max(relative_x, relative_y)
-        for i in range(0,max_step):
-            # send a step pluse
-            if relative_x > 0:
+		max_step = max(relative_x, relative_y)
+		for i in range(0,max_step):
+			if relative_x > 0:
 				GPIO.output(self.__y_dir_pin, y_dir)
-                # set_gpio(x_step_pin, HIGH)
-                relative_x -= 1
+				relative_x -= 1
 
-            if relative_y > 0:
+			if relative_y > 0:
 				GPIO.output(self.y_step_pin, y_dir)
-                # set_gpio(y_step_pin, HIGH)
-                relative_y -= 1
+				relative_y -= 1
+
 			time.sleep(0.2)
 			GPIO.output(self.x_step_pin, y_dir)
-            # set_gpio(x_step_pin, LOW)
 			GPIO.output(self.y_step_pin, y_dir)
-            # set_gpio(y_step_pin, LOW)
-            time.sleep(0.2)
-            # delay_us(100)
+			time.sleep(0.2)
 		
 
 if __name__ == '__main__':
